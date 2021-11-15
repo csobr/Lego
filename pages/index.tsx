bd9fac78afd6a865cb6f0ba3f444ef80bc710e6d
Siham Hadi <siham.hadi@live.se>
2021-11-15 22:50:06 +0100

    Add first version of model

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,22 @@
+import { OrbitControls, PerspectiveCamera, Sky } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import dynamic from 'next/dynamic';
+
+const Model = dynamic(() => import('../model/Lego_world'));
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="scene">
+      <Canvas>
+        <directionalLight />
+        <OrbitControls />
+        <directionalLight intensity={1} />
+        <Suspense fallback={null}>
+          <Model position={[0, 0, 0]} />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 };
 export default Home;
