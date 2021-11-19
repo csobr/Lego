dce04c89e71620b6ef623272ae3f5505672fa8ba
Siham Hadi <siham.hadi@live.se>
2021-11-19 09:43:01 +0100

    Remove cube camera and add texture to model

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import dynamic from 'next/dynamic';
-import { OrbitControls, Sky } from '@react-three/drei';
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import * as THREE from 'three';
 import React, { Suspense, useRef } from 'react';
+import dynamic from 'next/dynamic';
+import { OrbitControls } from '@react-three/drei';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+
 const Model = dynamic(() => import('../model/Lego_world'), {
   ssr: false
 });
@@ -18,25 +18,11 @@ const CameraControls = () => {
   return <OrbitControls ref={controls} args={[camera, domElement]} />;
 };
 
-function SkyBox() {
-  const { scene } = useThree();
-  scene.background = new THREE.CubeTextureLoader()
-    .setPath('img/')
-    .load([
-      'front.jpg',
-      'back.jpg',
-      'top.jpg',
-      'bottom.jpg',
-      'left.jpg',
-      'right.jpg'
-    ]);
-  return null;
-}
 const Home = () => {
   return (
     <div className="scene">
       <Canvas>
-        <SkyBox />
+        <ambientLight />
         <CameraControls />
         <Suspense fallback={null}>
           <Model position={[0, 0, 0]} />
